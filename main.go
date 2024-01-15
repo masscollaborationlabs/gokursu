dc303616a57e346b9cb52db6ffa57e6562917e8b
Mert Gör <mertgor@masscollabs.xyz>
2024-01-16 02:25:45 +0300

    maps example book = kitap

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"golesson/conditionals"
 	"golesson/functions"
 	"golesson/loops"
+	"golesson/maps"
 	"golesson/slices"
 	"golesson/variables"
 )
@@ -18,22 +19,24 @@ func main() {
 	arrays.Demo4()
 	slices.Demo2()
 	functions.SayHello("Mert Gör")
-	functions.Addition(2,6)
-	var total = functions.Addition(3,8)
+	functions.Addition(2, 6)
+	var total = functions.Addition(3, 8)
 	fmt.Println(total)
 	fmt.Println(total * 10)
-	
-	result1, result2, result3, result4 := functions.MathOps(10,2)
+
+	result1, result2, result3, result4 := functions.MathOps(10, 2)
 	fmt.Println("Add :", result1)
 	fmt.Println("Subtract :", result2)
 	fmt.Println("Multiply : ", result3)
 	fmt.Println("Divide : ", result4)
-	
-	var result = functions.AddVariadic(1,3,4,5,3)
+
+	var result = functions.AddVariadic(1, 3, 4, 5, 3)
 	fmt.Println(result)
 
-	fmt.Println(functions.AddVariadic(1,3,4))
+	fmt.Println(functions.AddVariadic(1, 3, 4))
 
-	numbers_main := []int{1,2,3,4}
+	numbers_main := []int{1, 2, 3, 4}
 	fmt.Println(functions.AddVariadic(numbers_main...))
+
+	maps.Demo1()
 }
---
+++ maps/demo1.go
@@ -0,0 +1,13 @@
+package maps
+
+import "fmt"
+
+func Demo1()  {
+	my_dictionary := make(map[string]string)
+	// key - value
+	my_dictionary["table"]="Masa"
+	my_dictionary["book"]="kitap"
+	my_dictionary["pencil"]="Kalem"
+	
+	fmt.Println(my_dictionary["book"])
+}
