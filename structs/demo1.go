9c1a8d8e99e4e382dc2de31ce06ced1736267e6b
Mert Gör <mertgor@masscollabs.xyz>
2024-01-20 14:48:38 +0300

    discountRate added to struct

--- structs/demo1.go
+++ structs/demo1.go
@@ -3,11 +3,12 @@ package structs
 import "fmt"
 
 func Demo1()  {
-	fmt.Println(product{"Computer", 5000, "XYZ"})
+	fmt.Println(product{"Computer", 5000, "XYZ", 20})
 }
 
 type product struct {
 	name string
 	unitPrice float64
 	brand string
+	discountRate int
 }
