25081c4c834dc07e5306c1938045be4a17c926a9
Mert Gör <mertgor@masscollabs.xyz>
2024-01-21 19:04:24 +0300

    fixed lower lower or equal to 10

--- goroutines/demo1.go
+++ goroutines/demo1.go
@@ -3,13 +3,13 @@ package goroutines
 import "fmt"
 
 func EvenNumber()  {
-	for i := 0; i < 10; i+=2 {
+	for i := 0; i <= 10; i+=2 {
 		fmt.Println("Even Number : ", i)
 	}
 }
 
 func OddNumber()  {
-	for i := 1; i < 10; i+=2 {
+	for i := 1; i <= 10; i+=2 {
 		fmt.Println("Odd Number : ", i)
 	}
 }
