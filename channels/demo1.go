db98168389f60a11aa42fee0a5b5914e1e3438a8
Mert Gör <mertgor@masscollabs.xyz>
2024-01-22 04:21:59 +0300

    time.sleep added to EvenNumber and OddNumber functions

--- channels/demo1.go
+++ channels/demo1.go
@@ -1,10 +1,18 @@
 package channels
 
+import (
+	"fmt"
+	"time"
+)
+
 func EvenNumber(EvenNumberCn chan int)  {
 	total := 0
 	for i := 0; i <= 10; i+=2 {
 		total = total + i
+		fmt.Println("Even Number is working")
+		time.Sleep(1 * time.Second)
 	}
+	
 	EvenNumberCn <- total
 }
 
@@ -12,6 +20,8 @@ func OddNumber(OddNumberCn chan int)  {
 	total := 0
 	for i := 1; i <= 10; i+=2 {
 		total = total + 1
+		fmt.Println("Odd Number is working")
+		time.Sleep(1 * time.Second)
 	}
 	OddNumberCn <- total
 }
