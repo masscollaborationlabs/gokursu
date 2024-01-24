8d115ed4ae43b7f896fabd36176742a4aa365f09
Mert Gör <mertgor@masscollabs.xyz>
2024-01-24 06:33:36 +0300

    Defer exeplained in more details

--- defer_statement/demo1.go
+++ defer_statement/demo1.go
@@ -6,8 +6,19 @@ func A()  {
 	fmt.Println("A function worked")
 }
 
+func C()  {
+	fmt.Println("C function worked")
+}
+
+func D()  {
+	fmt.Println("D function worked")
+}
+
 func B()  {
 	defer A()
+	defer C()
+	defer D()
 	fmt.Println("B function worked")
 }
+
 
