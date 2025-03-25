f61d0df67b870ae8e29396606e85f40fe7a38076
Mert Gör <mertgor@masscollabs.xyz>
2025-03-25 05:53:39 +0300

    writing int variable and writing and telling type of variable

--- main.go
+++ main.go
@@ -3,17 +3,20 @@ package main
 import "fmt"
 
 func main() {
-    
-    var text string = "Hello Golang World!"
-    
-    fmt.Println(text)
 
-    var vat int = 12
+	var text string = "Hello Golang World!"
 
-    fmt.Println(vat) 
+	fmt.Println(text)
 
-    var vat_float float64 = 12.2321
+	var vat int = 12
 
-    fmt.Println(vat_float) 
+	fmt.Println(vat)
 
+	var vat_float float64 = 12.2321
+
+	fmt.Println(vat_float)
+
+	vat_another := 25
+	fmt.Println(vat_another)
+	fmt.Printf("data type : %T\n", vat_another)
 }
